adc1b537c21ed60f099665ea7ec4c0e873119781
Лукьянов Павел <pahan-01.04.10@mail.ru>
2023-06-03 11:57:30 +0500

    fix menu btn

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     //Бургер меню
-    const btn = document.getElementById('menuBtn');
+    /* const btn = document.getElementById('menuBtn');
     let menu = document.getElementById('menuContainer');
-
+ 
     btn.addEventListener('click', showMenu);
-
+ 
     function showMenu() {
         menu.classList.toggle('menu-show');
         let toggle = document.querySelector('.nav-toggle');
         this.classList.toggle('opened');
-    }
+    } */
+
+    let menu = document.getElementById('menuContainer');
+    let menuBtn = document.querySelector('.menu-btn');
+    menuBtn.addEventListener('click', () => {
+        menuBtn.classList.toggle('open');
+        menu.classList.toggle('menu-show');
+    });
+
+
+
 });
